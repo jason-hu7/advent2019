92311b900b9a1c7481647c5e546fc299c1656d52
jason-hu7 <dcjasonhu@gmail.com>
2019-12-14 13:19:11 -0600

    day 4 part 2

--- day4_secure_container.py
+++ day4_secure_container.py
@@ -1,9 +1,7 @@
 from typing import Tuple
+import itertools
 
-
-
-
-def check_requirements(password: int) -> bool:
+def check_requirements(password: int, part2: bool = True) -> bool:
     password_str = str(password)
     # 1st requirement - 6 digits
     assert len(password_str) == 6
@@ -11,9 +9,17 @@ def check_requirements(password: int) -> bool:
     twin_neighbor = False
     never_decrease = True
 
+    # 2nd requirement - matching adjacent digits
+    running_twins = [sum(1 for _ in l) for n, l in itertools.groupby(password_str)]
+    if max(running_twins) >= 2 and not part2:
+        twin_neighbor = True
+    elif 2 in running_twins and part2:
+        twin_neighbor = True
+
+    # 3rd requirement - never decrease
     for i in range(1, len(password_str)):
-        if password_str[i] == password_str[i-1]:
-            twin_neighbor = True
+        # if password_str[i] == password_str[i-1]:
+        #     twin_neighbor = True
         if int(password_str[i]) < int(password_str[i-1]):
             never_decrease = False
 
@@ -23,10 +29,10 @@ def check_requirements(password: int) -> bool:
         return False
 
 
-def find_password(password_range: Tuple[int, int]) -> int:
+def find_password(password_range: Tuple[int, int], part2:bool = True) -> int:
     passwords = []
     for password in range(password_range[0], password_range[1]):
-        if check_requirements(password):
+        if check_requirements(password, part2):
             passwords.append(password)
         else:
             pass
@@ -39,9 +45,24 @@ if __name__ == "__main__":
     test2 = 223450
     test3 = 123789
 
-    assert check_requirements(test1)
-    assert not check_requirements(test2)
-    assert not check_requirements(test3)
+    assert check_requirements(test1, part2=False)
+    assert not check_requirements(test2, part2=False)
+    assert not check_requirements(test3, part2=False)
     
-    answer1 = find_password(password_range)
+    answer1 = find_password(password_range, part2=False)
     print(len(answer1))
+
+    # part 2 starts here
+    print("----------------------------------------part 2 starts here:")
+
+    test1 = 112233
+    test2 = 123444
+    test3 = 111122
+
+    assert check_requirements(test1, part2=True)
+    assert not check_requirements(test2, part2=True)
+    assert check_requirements(test3, part2=True)
+
+    answer2 = find_password(password_range, part2=True)
+    print(len(answer2))
+
