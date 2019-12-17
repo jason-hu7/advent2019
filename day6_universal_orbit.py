504e9fd7e30050e353dcd02088cc11e9abbeefac
jason-hu7 <dcjasonhu@gmail.com>
2019-12-16 23:36:13 -0600

    day6 part 1

--- day6_universal_orbit.py
+++ day6_universal_orbit.py
@@ -6,8 +6,8 @@ def get_direct_orbits(orbits: List[str]) -> int:
 def get_indirect_orbits(orbits: List[str]) -> int:
     orbit_map = construct_orbit_map(orbits)
     total_indirect_orbits = 0
-    for key, connections in orbit_map.values():
-        total_indirect_orbits += len(connections)
+    for key, connections in orbit_map.items():
+        total_indirect_orbits += len(connections) - 1  # minus 1 because ignore direct
     return total_indirect_orbits
 
 def construct_orbit_map(orbits: List[str]) -> Dict[str, List[str]]:
@@ -24,11 +24,11 @@ def construct_orbit_map(orbits: List[str]) -> Dict[str, List[str]]:
             print("something might be wrong")
         else:
             orbitee = orbitees[i]
-            orbit_map[orbiter] = orbitee
+            orbit_map[orbiter] = [orbitee]
             new_orbitee = orbitee
             while new_orbitee in orbiters:
-                orbiter = new_orbitee
-                orbiter_index = orbiters.index[orbiter]
+                new_orbiter = new_orbitee
+                orbiter_index = orbiters.index(new_orbiter)
                 new_orbitee = orbitees[orbiter_index]
                 orbit_map[orbiter].append(new_orbitee)
     return orbit_map
@@ -42,5 +42,8 @@ if __name__ == "__main__":
         orbits = f.read().splitlines()
 
     test1 = ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"]
+    # print(get_indirect_orbits(test1))
     assert (get_direct_orbits(test1)+get_indirect_orbits(test1)) == 42
 
+    total_relations = get_direct_orbits(orbits)+get_indirect_orbits(orbits)
+    print("part 1 answer is: ", total_relations)
