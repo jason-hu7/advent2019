267d336b1959c71b6a69afd9162378f6a290f0b9
jason-hu7 <dcjasonhu@gmail.com>
2019-12-17 00:30:30 -0600

    day6 part2

--- day6_universal_orbit.py
+++ day6_universal_orbit.py
@@ -1,14 +1,18 @@
 from typing import List, Tuple, Dict
 
+
 def get_direct_orbits(orbits: List[str]) -> int:
     return len(set(orbits))
 
+
 def get_indirect_orbits(orbits: List[str]) -> int:
     orbit_map = construct_orbit_map(orbits)
+    # print(orbit_map)
     total_indirect_orbits = 0
-    for key, connections in orbit_map.items():
+    for _, connections in orbit_map.items():
         total_indirect_orbits += len(connections) - 1  # minus 1 because ignore direct
     return total_indirect_orbits
+
 
 def construct_orbit_map(orbits: List[str]) -> Dict[str, List[str]]:
     orbiters = []
@@ -17,7 +21,7 @@ def construct_orbit_map(orbits: List[str]) -> Dict[str, List[str]]:
         orbitee, orbiter = orbit.split(")")
         orbiters.append(orbiter)
         orbitees.append(orbitee)
-    
+
     orbit_map = {}
     for i, orbiter in enumerate(orbiters):
         if orbiter in orbit_map:
@@ -34,16 +38,66 @@ def construct_orbit_map(orbits: List[str]) -> Dict[str, List[str]]:
     return orbit_map
 
 
-
+def get_min_orbital_transfers(orbits: List[str]) -> int:
+    orbit_map = construct_orbit_map(orbits)
+    min_steps = 0
+    you_path = orbit_map["YOU"]
+    san_path = orbit_map["SAN"]
+    # print(you_path, san_path)
+    for i in you_path:
+        j_to_common = 0
+        for j in san_path:
+            if i == j:
+                # found the common connector
+                return min_steps + j_to_common
+            else:
+                j_to_common += 1
+        # if didn't find common connector step + 1 for YOU
+        min_steps += 1
+    return None
 
 if __name__ == "__main__":
     # read data
     with open("data/input_day6.txt") as f:
         orbits = f.read().splitlines()
 
-    test1 = ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"]
+    test1 = [
+        "COM)B",
+        "B)C",
+        "C)D",
+        "D)E",
+        "E)F",
+        "B)G",
+        "G)H",
+        "D)I",
+        "E)J",
+        "J)K",
+        "K)L",
+    ]
     # print(get_indirect_orbits(test1))
-    assert (get_direct_orbits(test1)+get_indirect_orbits(test1)) == 42
+    assert (get_direct_orbits(test1) + get_indirect_orbits(test1)) == 42
 
-    total_relations = get_direct_orbits(orbits)+get_indirect_orbits(orbits)
+    total_relations = get_direct_orbits(orbits) + get_indirect_orbits(orbits)
     print("part 1 answer is: ", total_relations)
+
+    print("-----------------------------------------------part 2 starts here.")
+    test2 = [
+        "COM)B",
+        "B)C",
+        "C)D",
+        "D)E",
+        "E)F",
+        "B)G",
+        "G)H",
+        "D)I",
+        "E)J",
+        "J)K",
+        "K)L",
+        "K)YOU",
+        "I)SAN",
+    ]
+
+    # print(get_min_orbital_transfers(test2))
+    assert (get_min_orbital_transfers(test2) == 4)
+
+    print("part 2 answer is: ", get_min_orbital_transfers(orbits))
