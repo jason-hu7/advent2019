547e2027c49065c84738a6ff4ab7fdadf71779ae
jason-hu7 <dcjasonhu@gmail.com>
2020-01-05 23:13:52 -0600

    day 10 part 1

--- day10_monitoring_station.py
+++ day10_monitoring_station.py
@@ -3,11 +3,19 @@ from typing import List, Dict, Tuple
 from itertools import permutations
 
 
+def find_best(space_map: np.ndarray):
+    asteroid_coords = find_asteroids(space_map)
+    direct_connections = get_direct_connections(asteroid_coords)
+    # print(direct_connections)
+    best_loc = max(direct_connections, key=direct_connections.get)
+    direct_sights = direct_connections[best_loc]
+    print(direct_sights)
+    return best_loc
+
+
 def load_tests(input_file: str = "data/test_day10.txt") -> Dict:
     tests = {}
     with open(input_file) as f:
-        # files = f.read()
-        # print(files.split('\n'))
         for line in f:
             if "test" in line:
                 key = line.strip("\n")
@@ -24,7 +32,7 @@ def find_asteroids(space_map: np.ndarray) -> List[Tuple[int, int]]:
     for y in range(space_map.shape[0]):
         for x in range(space_map.shape[1]):
             if space_map[y][x] == "#":
-                asteroid_coords.append((y, x))
+                asteroid_coords.append((x, y))
     return asteroid_coords
 
 
@@ -51,23 +59,29 @@ def examine_line(
     asteroids_list = asteroid_coords.copy()
     asteroids_list.remove(asteroid1)
     asteroids_list.remove(asteroid2)
-    y1, x1 = asteroid1
-    y2, x2 = asteroid2
-    
-def distance(
-    asteroid1: Tuple[int, int],
-    asteroid2: Tuple[int, int]
-) -> int:
-    y1, x1 = asteroid1
-    y2, x2 = asteroid2
+    # print("examine line between {} and {}".format(asteroid1, asteroid2))
+    for asteroid3 in asteroids_list:
+        if is_between(asteroid1, asteroid2, asteroid3):
+            # print("{} is in the way".format(asteroid3))
+            return False
+    return True
+
+
+def distance(asteroid1: Tuple[int, int], asteroid2: Tuple[int, int]) -> int:
+    x1, y1 = asteroid1
+    x2, y2 = asteroid2
     return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
+
 def is_between(
-    asteroid1: Tuple[int, int],
-    asteroid2: Tuple[int, int],
-    asteroid3: Tuple[int, int],
+    asteroid1: Tuple[int, int], asteroid2: Tuple[int, int], asteroid3: Tuple[int, int],
 ) -> bool:
-    return distance(asteroid1, asteroid3) + distance(asteroid3, asteroid2) == distance(asteroid1, asteroid2) 
+    e = 0.00001
+    sum_13_23 = distance(asteroid1, asteroid3) + distance(asteroid3, asteroid2)
+    if abs(sum_13_23 - distance(asteroid1, asteroid2)) > e:
+        return False
+    else:
+        return True
 
 
 
@@ -78,4 +92,21 @@ if __name__ == "__main__":
     )
 
     tests = load_tests()
-    print(tests)
+
+    # best_loc = find_best(tests["test1"])
+    # assert best_loc == (3, 4)
+
+    # best_loc = find_best(tests["test2"])
+    # assert best_loc == (5, 8)
+
+    # best_loc = find_best(tests["test3"])
+    # assert best_loc == (1, 2)
+
+    # best_loc = find_best(tests["test4"])
+    # assert best_loc == (6, 3)
+
+    # best_loc = find_best(tests["test5"])
+    # assert best_loc == (11, 13)
+
+    best_loc = find_best(space_map)
+    print(best_loc)
